aea5842ad142345475ed4501ad3073c7bf8591ee
priya <priyatimcy1123@gmail.com>
2026-01-13 14:22:33 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+fetch('https://api.yourdomain.com/users')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("data").innerHTML =
+      "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  });
+
+frontend/package.json
